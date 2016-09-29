d65c83f58c086ab5c08eb5b1db08ccc5713178b5
TotallyWrong <lin2jack@gmail.com>
2016-09-29 16:16:09 +0800

    astyle update

--- math-toolkit.h
+++ math-toolkit.h
@@ -34,20 +34,20 @@ inline __attribute__((always_inline))
 void subtract_vector(const double *a, const double *b, double *out)
 {
     //for (int i = 0; i < 3; i++)
-        //out[i] = a[i] - b[i];
-          out[0] = a[0] - b[0];
-          out[1] = a[1] - b[1];
-          out[2] = a[2] - b[2];
+    //out[i] = a[i] - b[i];
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
 }
 
 inline __attribute__((always_inline))
 void multiply_vectors(const double *a, const double *b, double *out)
 {
     //for (int i = 0; i < 3; i++)
-        //out[i] = a[i] * b[i];
-          out[0] = a[0] * b[0];
-          out[1] = a[1] * b[1];
-          out[2] = a[2] * b[2];
+    //out[i] = a[i] * b[i];
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    out[2] = a[2] * b[2];
 }
 
 //static inline
@@ -55,10 +55,10 @@ inline __attribute__((always_inline))
 void multiply_vector(const double *a, double b, double *out)
 {
     //for (int i = 0; i < 3; i++)
-        //out[i] = a[i] * b;
-          out[0] = a[0] * b;
-          out[1] = a[1] * b;
-          out[2] = a[2] * b;
+    //out[i] = a[i] * b;
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
 }
 
 inline __attribute__((always_inline))
@@ -75,8 +75,8 @@ double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
     //for (int i = 0; i < 3; i++)
-        //dp += v1[i] * v2[i];
-          dp= v1[0] * v2[0]+v1[1] * v2[1]+v1[2] * v2[2];
+    //dp += v1[i] * v2[i];
+    dp= v1[0] * v2[0]+v1[1] * v2[1]+v1[2] * v2[2];
     return dp;
 }
 
